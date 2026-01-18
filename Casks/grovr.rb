48f87c0fed077895e04122acde5d6cc922ff4411
Jaewon Kim <jak3b.kim@gmail.com>
2026-01-18 13:20:07 +0000

    Sync grovr cask

--- Casks/grovr.rb
+++ Casks/grovr.rb
@@ -2,12 +2,13 @@ cask "grovr" do
   version "0.7.0"
   sha256 "PLACEHOLDER_UPDATE_ON_RELEASE"
 
-  url "https://github.com/j1king/grovr/releases/download/v#{version}/Grovr_#{version}_universal.dmg"
+  url "https://github.com/j1king/grovr/releases/download/v#{version}/Grovr_#{version}_aarch64.dmg"
   name "Grovr"
   desc "Desktop Git worktree manager"
   homepage "https://github.com/j1king/grovr"
 
   depends_on macos: ">= :catalina"
+  depends_on arch: :arm64
   auto_updates true
 
   app "Grovr.app"
