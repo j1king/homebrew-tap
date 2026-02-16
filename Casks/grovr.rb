9613b414a95a62f57637f2c977e2f548e388abd9
Jaewon Kim <jak3b.kim@gmail.com>
2026-02-16 05:24:35 +0000

    Sync grovr cask

--- Casks/grovr.rb
+++ Casks/grovr.rb
@@ -1,6 +1,6 @@
 cask "grovr" do
-  version "0.7.2"
-  sha256 "6ce23832119671ee12fb046303e12a1b22a3a489d56c8728e73fd83e742daf9f"
+  version "0.7.3"
+  sha256 "7f8fa4f49c8597a9aa598d129e89babd14b3361300c50c0e93a989de409deaed"
 
   url "https://github.com/j1king/grovr/releases/download/v#{version}/Grovr_#{version}_aarch64.dmg"
   name "Grovr"
